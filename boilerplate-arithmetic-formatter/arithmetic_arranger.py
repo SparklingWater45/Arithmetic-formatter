cb04eb3e486b8a41a48c3504ee3f94a082d42e37
Dylan van Heerden <dylanvh45@gmail.com>
2021-03-08 11:03:13 +0200

    Output works, commiting before moving onto fixing the error checks , return value isnt registering and code is running even if error in check

--- boilerplate-arithmetic-formatter/arithmetic_arranger.py
+++ boilerplate-arithmetic-formatter/arithmetic_arranger.py
@@ -1,9 +1,18 @@
 def arithmetic_arranger(problems,solve=False):
+        
+    def errors(problems):
 
+       for i, problem in enumerate(problems):
+        num_top, operand, num_bot = problem.split()
 
-    def errors(problems,solve):
-        if len(problems) > 5:
-            return "Error: Too many problems."
+        if not operand in ["+", "-"]:
+            return "Error: Operator must be '+' or '-'."
+
+        if not num_top.isdigit() or not num_bot.isdigit():
+            return "Error: Numbers must only contain digits."
+
+        if len(num_top) > 4 or len(num_bot) > 4:
+            return "Error: Numbers cannot be more than four digits."
 
     def split_input(problems):
         temp = ''
@@ -21,6 +30,28 @@ def arithmetic_arranger(problems,solve=False):
         return arr_numbers,arr_operands
         
 
+    def determine_sum(arr_numbers,arr_operands):
+        arr_inttopnums = []
+        arr_intbotnums = []
+        arr_sum = []
+        for i in range(len(arr_numbers)): #split top and bottom strings into integers
+            arr_inttopnums.append(int(arr_numbers[i][0]))
+            arr_intbotnums.append(int(arr_numbers[i][1]))
+
+        for i in range(len(arr_numbers)):
+            if arr_operands[i] == '+':
+                arr_sum.append(arr_inttopnums[i] + arr_intbotnums[i])
+            else:
+                arr_sum.append(arr_inttopnums[i] - arr_intbotnums[i])
+        
+
+        for i in range(len(arr_sum)):
+            arr_sum[i] = str(arr_sum[i]) #change back to str
+        
+        
+
+        return arr_sum
+
 
     def determine_dash(numbers):  # takes in array of the two numbers
     #dashes used in calculations are always 2 more than longest number
@@ -36,11 +67,11 @@ def arithmetic_arranger(problems,solve=False):
             arr_dash.append(calculate(i))
             
         return arr_dash
-
-    def determine_spaces(arr_numbers,arr_dash):
+    
+    def determine_spaces(arr_numbers,arr_dash,arr_sum):
         #spit arrays in top and bottom
         #minus num[i] from len dash[i] of same index
-        #left over number = arr_spaces[i]
+        #left over number = arr_spaces[i]s
         
         arr_topnums = []
         arr_botnums = [] 
@@ -53,19 +84,28 @@ def arithmetic_arranger(problems,solve=False):
         arr_botlen = []
         arr_topspaces = []
         arr_bottomspaces = []
+        arr_sumlen = []
+        arr_sumspaces = []
 
         for i in range(len(arr_topnums)): #find the length of spaces in numbers
             arr_toplen.append(len(arr_dash[i]) - len(arr_topnums[i]))
             arr_botlen.append(len(arr_dash[i]) - len(arr_botnums[i])-1) #-1 for operand taking 1 space positon
+            
+            if '-' not in arr_sum[i]:
+                arr_sumlen.append(len(arr_dash[i]) - len(arr_sum[i]))
+            else:
+                arr_sumlen.append(len(arr_dash[i]) - len(arr_sum[i])) #for negative numbers
+
+
 
         for i in range(len(arr_toplen)): #convert spaces in numbers into spaces
             arr_topspaces.append(' '*arr_toplen[i])
             arr_bottomspaces.append(' '*arr_botlen[i])
-        
+            arr_sumspaces.append(' '* arr_sumlen[i])
 
-        return arr_topspaces,arr_bottomspaces,arr_topnums,arr_botnums #return array of spaces for line 1 and 2
+        return arr_topspaces,arr_bottomspaces,arr_sumspaces,arr_topnums,arr_botnums #return array of spaces for line 1 and 2
     
-    def display_output(arr_topspaces, arr_bottomspaces, arr_topnums, arr_bottomnums):
+    def display_output(arr_topspaces, arr_bottomspaces,arr_sumspaces, arr_topnums, arr_bottomnums,arr_sum):
         line1 = ''  # top numbers
         line2 = ''  # operand and bottom numbers
         line3 = ''  # dashes
@@ -75,33 +115,39 @@ def arithmetic_arranger(problems,solve=False):
             line1 = line1 +  arr_topspaces[i] +arr_topnums[i] + '    '
             line2 = line2 + arr_operands[i]+ arr_bottomspaces[i] + arr_bottomnums[i] + '    '
             line3 = line3 + arr_dash[i] + '    '
+            line4 = line4 + arr_sumspaces[i] + arr_sum[i] + '    '
 
-        line1 = line1 #+ '\n'
+        line1 = line1 #+ '\n''
         line2 = line2 #+ '\n'
         line3 = line3 #+ '\n'
-        final_output = line1 +'\n' + line2 + '\n' + line3 + '\n'
+        final_output = line1 +'\n' + line2 + '\n' + line3
 
-        # print(line1)
-        # print(line2)
-        # print(line3)
-        print(final_output)
+        if solve == False: #if no sum required
+            return final_output
+        else:
+            final_output = final_output + '\n' + line4
+            return final_output
+            
 
-
+  
     '''
     ----------------
     Start of program
     ----------------
     '''
-    arr_numbers,arr_operands = split_input(problems) #splits the initial string for use
-    arr_dash = determine_dash(arr_numbers) #determine amount of dashes for each sum
-    arr_topspaces,arr_bottomspaces,arr_topnums,arr_bottomnums = determine_spaces(arr_numbers,arr_dash) #determine spaces infront of numbers
-    display_output(arr_topspaces, arr_bottomspaces,arr_topnums, arr_bottomnums) #display output
+    error = ''
+    error = errors(problems)  # if this returns something it doesnt stop the program
+    if error != '':
+        arr_numbers,arr_operands = split_input(problems) #splits the initial string for use
+        arr_dash = determine_dash(arr_numbers) #determine amount of dashes for each sum
+        arr_sum = determine_sum(arr_numbers, arr_operands)  # calculate arrsum
+        arr_topspaces,arr_bottomspaces,arr_sumspaces,arr_topnums,arr_bottomnums = determine_spaces(arr_numbers,arr_dash,arr_sum) #determine spaces infront of numbers
+        arranged_problems = display_output(arr_topspaces, arr_bottomspaces,arr_sumspaces,arr_topnums, arr_bottomnums,arr_sum) #display output
+        return arranged_problems
+    else:
+        return error
+
+# only + or /#
+print(arithmetic_arranger(["3 + 855", "3801 + 2", "45 - 43", "123 - 49"],True))
 
 
-            
-
-
-    # errors(problems,solve)
-    # return arranged_problems
-    
-arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])
